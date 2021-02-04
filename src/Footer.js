e7d2734e6f7d8e5c77c4de4546f6a89ec6867302
LilianeTop <hallo@lilianetop.nl>
2021-02-04 20:20:32 +0100

    diverse animaties toegevoegd

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,14 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-  <p> fotografie&artwork©elsenschede webdesign&webdevelopment©lilianetop
+  return(
 
----- Voor bezichtiging van kunstwerken bel: 06-1234 1234 of mail: els@wiljedit.nl</p>
+    <p> <hr /> fotografie&artwork©elsenschede webdesign&webdevelopment©lilianetop
+  ---- Voor bezichtiging van kunstwerken bel: 06-1234 1234 of mail: els@wiljedit.nl </p>
+
 );
+
+
 }
 
 export default Footer;
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,12 +3,10 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
 
-      <nav class="navbar navbar-expand-lg navbar-dark bg-color=#354440 fixed-top py-lg-0">
-
+      <nav class="navbar navbar-expand-lg navbar-dark bg-color=#354440  py-lg-0">
         <a class="navbar-brand" href="#">
           <img src="img/logoInverse.png"  width="50" heigth="50" alt="logo"/>
         </a>
-
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
