1031446b440c68895d91e446ddd13d494f7fbb35
LilianeTop <hallo@lilianetop.nl>
2021-02-07 19:46:22 +0100

    tags aangepast

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return(
     <div className="fixed-bottom">
       <hr />
-      <p> Voor bezichtiging van kunstwerken bel: 06-5492 2247 of mail: els.enschede@outlook.com </p>
+      <p> Voor bezichtiging van kunstwerken bel: 06-5492 2247 of mail: els.enschede@xs4all.nl </p>
       <p>
         <span className="copyright1"> © 2021 Els Enschede</span>
         <span className="copyright2">Webdesign & Development door Liliane Top</span>
--- src/List.js
+++ src/List.js
@@ -6,12 +6,12 @@ const List = (props) => {
     <div>
         <h2> {props.title} </h2>
           <div className = "list">
-            <Foto picture="/img/Natuur.jpg" alt="groen water"/>
-            <Foto picture="/img/mensen3Mannen.jpg" alt="3 dronken mannen op een bankje"/>
-            <Foto picture="/img/Poppenkoppen.jpg" alt="poppenkop upclose"/>
-            <Foto picture="/img/Industrieel.jpg" alt="schip aambeeld upclose"/>
-            <Foto picture="/img/Abstract.jpg" alt="abstract rood"/>
-            <Foto picture="/img/Scenes.jpg" alt="kip en clown op kei"/>
+            <Foto picture="/img/Natuur.jpg" alt="Slootje"/>
+            <Foto picture="/img/mensen3Mannen.jpg" alt="Ladderzat"/>
+            <Foto picture="/img/Poppenkoppen.jpg" alt="Dit is Ambra"/>
+            <Foto picture="/img/Industrieel.jpg" alt="Scheepsdeel"/>
+            <Foto picture="/img/Abstract.jpg" alt="Afgebladderd"/>
+            <Foto picture="/img/Scenes.jpg" alt="Hanig Type"/>
           </div>
     </div>
 );
