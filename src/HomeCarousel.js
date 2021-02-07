18311adbe6af4f7b92802c4a0d044e4e714ffdc4
LilianeTop <hallo@lilianetop.nl>
2021-02-07 17:59:23 +0100

    waterstroom aangepast

--- src/HomeCarousel.js
+++ src/HomeCarousel.js
@@ -6,16 +6,6 @@ import "./HomeCarousel.css";
 class HomeCarousel extends React.Component {
 
     render() {
-        const waterrunningfotos =
-            [{
-                src: "img/homeRunningWater.jpg",
-                alt: "waterfoto"
-            },
-                {
-                    src: "img/homeRunningWaterMirror.jpg",
-                    alt: "waterfotomirror"
-                }];
-
         return (
             <div className='wrapper'>
                 <div className='sliding-background'>
