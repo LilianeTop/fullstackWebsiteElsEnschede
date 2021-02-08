db024de49b1508d1568a5f5581e1a1e172c706ab
LilianeTop <hallo@lilianetop.nl>
2021-02-08 18:52:45 +0100

    aanpassen foto themas homepage

--- src/List.js
+++ src/List.js
@@ -6,15 +6,15 @@ const List = (props) => {
     <div>
         <h2> {props.title} </h2>
           <div className = "list">
-            <Foto picture="/img/Natuur.jpg" alt="Landschap"/>
+            <Foto picture="/img/Verborgen citroenen.jpg" alt="Landschap"/>
             <Foto picture="/img/mensen3Mannen.jpg" alt="Mensen"/>
-            <Foto picture="/img/Poppenkoppen.jpg" alt="Reizen"/>
+            <Foto picture="/img/Reizen.jpg" alt="Reizen"/>
             <Foto picture="/img/Industrieel.jpg" alt="Industrie"/>
             <Foto picture="/img/Abstract.jpg" alt="Abstract"/>
             <Foto picture="/img/Scenes.jpg" alt="Scenes"/>
             <Foto picture="/img/Industrieel.jpg" alt="Water"/>
-            <Foto picture="/img/Abstract.jpg" alt="Buiten"/>
-            <Foto picture="/img/Scenes.jpg" alt="Stad"/>
+            <Foto picture="/img/Buiten.jpg" alt="Buiten"/>
+            <Foto picture="/img/Stad.jpg" alt="Stad"/>
           </div>
     </div>
 );
